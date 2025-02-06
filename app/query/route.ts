481ab051488cdd3a25e8e02cfedf374a84aa96be
Ben Phillips <pben012@gmail.com>
2025-02-06 21:46:47 +1100

    learnt about streaming, loading pages, route groups, fetching and passing data

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,27 +1,6 @@
 import { db } from "@vercel/postgres";
-import { Revenue } from "../lib/definitions";
 
 const client = await db.connect();
-
-export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-    console.log('Fetching revenue data...');
-    console.log(process.env.POSTGRES_URL)
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await client.sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
-}
 
 async function listInvoices() {
 	const data = await client.sql`
